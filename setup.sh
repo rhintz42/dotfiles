c8541864e6afd0ab041d62c0a4c3a05a09fa0dbf
Robert Hintz <roberthintz@gmail.com>
2014-07-19 15:42:36 -0700

    Add setting the git configuration to simple to adopt the new behavior for pushing

--- setup.sh
+++ setup.sh
@@ -38,3 +38,4 @@ ln -s $(pwd)/.Xmodmap ~/.Xmodmap
 git config --global user.name "Robert Hintz"
 git config --global user.email roberthintz@gmail.com
 git config --global color.ui true
+git config --global push.default simple
